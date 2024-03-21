ec38af32739c16ca07403f2f4e774b130dce7cc7
LAPTOP-I04HNM2I\kim <woong2209@gmail.com>
2024-03-21 17:05:35 +0900

    24.03.21(THU) 0.2.3.1 OAuth2SignInController 세션에 멤버 등급 저장하게 변경

--- src/main/java/infinitystone/chalKag/controller/common/OAuth2SignInController.java
+++ src/main/java/infinitystone/chalKag/controller/common/OAuth2SignInController.java
@@ -1,18 +1,24 @@
 package infinitystone.chalKag.controller.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import infinitystone.chalKag.biz.customOAuth2.CustomOAuth2User;
+import infinitystone.chalKag.biz.member.MemberDTO;
+import infinitystone.chalKag.biz.member.MemberService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class OAuth2SignIn {
+public class OAuth2SignInController {
+	
+	@Autowired
+	private MemberService memberService;
 
 	@RequestMapping("/oauth2SignIn")
-	public String myEndpoint(HttpSession session) {
+	public String myEndpoint(HttpSession session, MemberDTO memberDTO) {
 
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -25,9 +31,16 @@ public class OAuth2SignIn {
 			String email = customOAuth2User.getEmail();
 
 			// 사용자 정보 반환
-			System.out.println("email : " + email);
+			System.out.println("[OAuth2SignInController] email : " + email);
 			
-			session.setAttribute("member", email);
+			memberDTO.setSearchCondition("OAuth2SignIn");
+			memberDTO.setMemberId(email);
+			
+			// 기존에 있는 멤버 서비스를 활용하여 ID와 해당 유저의 등급을 세션에 저장
+			MemberDTO result = memberService.selectOne(memberDTO);
+			
+			session.setAttribute("member", result.getMemberId());
+			session.setAttribute("memberGrade", result.getMemberGrade());
 			
 		} 
 		return "redirect:/main";
