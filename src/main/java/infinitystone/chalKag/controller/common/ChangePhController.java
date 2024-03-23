4edd1dbe7ef29f71b316cc51617c731021d6bd21
plzjun0731 <plzjun0731@naver.com>
2024-03-23 22:07:11 +0900

    24.03.23 (SUN) 0.2.1.6 myPage 정보 변경 컨트롤러 기능 구현

--- src/main/java/infinitystone/chalKag/controller/common/ChangePhController.java
+++ src/main/java/infinitystone/chalKag/controller/common/ChangePhController.java
@@ -9,35 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ChangeNicknameController {
+public class ChangePhController {
 
   @Autowired
   public MemberService memberService;
 
-  @RequestMapping(value = "/changeNickname", method = RequestMethod.GET)
-  public String changeNicknamePage(HttpSession session) {
-
-    if (session.getAttribute("memberGrade") != "premium") {
-      return "redirect:myPage";
-    }
-
-    return "changeNickname";
+  @RequestMapping(value = "/changePh", method = RequestMethod.GET)
+  public String changePhPage() {
+    return "changePh";
   }
 
-  @RequestMapping(value = "/changeNickname", method = RequestMethod.POST)
-  public String changeNickname(MemberDTO memberDTO, HttpSession session) {
+  @RequestMapping(value = "/changePh", method = RequestMethod.POST)
+  public String changePh(MemberDTO memberDTO, HttpSession session) {
 
     memberDTO.setMemberId((String) session.getAttribute("member"));
-    memberDTO.setSearchCondition("changeNickname");
+    memberDTO.setSearchCondition("changePh");
 
-    System.out.println("ChangeNicknameController In로그");
+    System.out.println("ChangePhController In로그 = [" + memberDTO + "]");
 
     if (!memberService.update(memberDTO)) {
       System.out.println("[로그] Controller");
       return "redirect:error";
     }
 
-    System.out.println("ChangeNicknameController Out로그");
+    System.out.println("ChangePhController Out로그");
 
     return "redirect:myPage";
   }
