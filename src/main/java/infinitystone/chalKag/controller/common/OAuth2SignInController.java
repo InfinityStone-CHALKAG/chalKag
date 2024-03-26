6c46a95d362f8d4f9c88afb8b0294e991a63ce17
plzjun0731 <plzjun0731@naver.com>
2024-03-26 09:08:31 +0900

    24.03.26 (TUE) 0.2.1.18 Log 경로 수정

--- src/main/java/infinitystone/chalKag/controller/common/OAuth2SignInController.java
+++ src/main/java/infinitystone/chalKag/controller/common/OAuth2SignInController.java
@@ -1,5 +1,7 @@
 package infinitystone.chalKag.controller.common;
 
+import infinitystone.chalKag.biz.signInLog.SignInLogDTO;
+import infinitystone.chalKag.biz.signInLog.SignInLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,50 +11,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import infinitystone.chalKag.biz.customOAuth2.CustomOAuth2User;
 import infinitystone.chalKag.biz.member.MemberDTO;
 import infinitystone.chalKag.biz.member.MemberService;
-import infinitystone.chalKag.biz.signinlog.SignInLogDTO;
-import infinitystone.chalKag.biz.signinlog.SignInLogService;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class OAuth2SignInController {
-	
-	@Autowired
-	private MemberService memberService;
-	
-	@Autowired
-	private SignInLogService signInLogService;
 
-	@RequestMapping("/oauth2SignIn")
-	public String myEndpoint(HttpSession session, MemberDTO memberDTO, SignInLogDTO signInLogDTO) {
+  @Autowired
+  private MemberService memberService;
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+  @Autowired
+  private SignInLogService signInLogService;
 
-		// 인증된 사용자가 CustomOAuth2User 객체인지 확인
-		if (authentication.getPrincipal() instanceof CustomOAuth2User) {
-			// CustomOAuth2User 객체로 캐스팅하여 사용자 정보에 접근
-			CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+  @RequestMapping("/oauth2SignIn")
+  public String myEndpoint(HttpSession session, MemberDTO memberDTO, SignInLogDTO signInLogDTO) {
 
-			// 사용자 정보 활용
-			String email = customOAuth2User.getEmail();
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-			// 사용자 정보 반환
-			System.out.println("[OAuth2SignInController] email : " + email);
-			
-			memberDTO.setSearchCondition("OAuth2SignIn");
-			memberDTO.setMemberId(email);
-			
-			// 기존에 있는 멤버 서비스를 활용하여 ID와 해당 유저의 등급을 세션에 저장
-			MemberDTO result = memberService.selectOne(memberDTO);
-			
-			signInLogDTO.setMemberId(result.getMemberId());
-			signInLogService.insert(signInLogDTO);
-			
-			session.setAttribute("member", result.getMemberId());
-			session.setAttribute("memberGrade", result.getMemberGrade());
-			
-		} 
-		return "redirect:/main";
+    // 인증된 사용자가 CustomOAuth2User 객체인지 확인
+    if (authentication.getPrincipal() instanceof CustomOAuth2User) {
+      // CustomOAuth2User 객체로 캐스팅하여 사용자 정보에 접근
+      CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-	}
+      // 사용자 정보 활용
+      String email = customOAuth2User.getEmail();
+
+      // 사용자 정보 반환
+      System.out.println("[OAuth2SignInController] email : " + email);
+
+      memberDTO.setSearchCondition("OAuth2SignIn");
+      memberDTO.setMemberId(email);
+
+      // 기존에 있는 멤버 서비스를 활용하여 ID와 해당 유저의 등급을 세션에 저장
+      MemberDTO result = memberService.selectOne(memberDTO);
+
+      signInLogDTO.setMemberId(result.getMemberId());
+      signInLogService.insert(signInLogDTO);
+
+      session.setAttribute("member", result.getMemberId());
+      session.setAttribute("memberGrade", result.getMemberGrade());
+
+    }
+    return "redirect:/main";
+
+  }
 
 }
